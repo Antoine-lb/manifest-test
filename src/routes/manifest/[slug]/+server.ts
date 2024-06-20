d53e561ba3b3e19a71ade0b8addbe92e803e5e39
Antoine LB <antoine.stc25@gmail.com>
2024-06-20 12:15:38 -0400

    more stuff

--- src/routes/manifest/[slug]/+server.ts
+++ src/routes/manifest/[slug]/+server.ts
@@ -3,18 +3,18 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async (event) => {
 	return json({
-		name: event.params.slug,
-		short_name: event.params.slug,
+		name: `Name ${event.params.slug}`,
+		short_name: `S ${event.params.slug}`,
 		description: 'My Awesome App description',
 		theme_color: '#ffffff',
 		icons: [
 			{
-				src: 'pwa-192x192.png',
+				src: '192.png',
 				sizes: '192x192',
 				type: 'image/png'
 			},
 			{
-				src: 'pwa-512x512.png',
+				src: '512.png',
 				sizes: '512x512',
 				type: 'image/png'
 			}
