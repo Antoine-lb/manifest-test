fcd7d81a4e1e591ccfe5ff71ffd3e003b379b1e9
Antoine LB <antoine.stc25@gmail.com>
2024-06-20 11:44:36 -0400

    header and icons

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -2,4 +2,14 @@
 	export let data;
 </script>
 
+<svelte:head>
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<title>{data.slug} Page</title>
+	<meta name="description" content={`${data.slug} Description`} />
+	<link rel="icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+	<link rel="mask-icon" href="/mask-icon.svg" color="#FFFFFF" />
+	<meta name="theme-color" content="#ffffff" />
+</svelte:head>
+
 <h1>{data.slug}</h1>
