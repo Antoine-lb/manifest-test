d2d80980271c6438349992295338dec5ddd6dd09
Antoine LB <antoine.stc25@gmail.com>
2024-06-20 12:54:06 -0400

    pwd

--- src/routes/manifest/[slug]/+server.ts
+++ src/routes/manifest/[slug]/+server.ts
@@ -3,21 +3,36 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async (event) => {
 	return json({
-		name: `Name ${event.params.slug}`,
-		short_name: `S ${event.params.slug}`,
-		description: 'My Awesome App description',
-		theme_color: '#ffffff',
+		background_color: '#EFEFF4',
+		theme_color: '#EFEFF4',
+		name: 'Pizzo Admin',
+		short_name: 'Pizzo',
+		display: 'standalone',
+		start_url: '/',
 		icons: [
 			{
-				src: '192.png',
-				sizes: '192x192',
-				type: 'image/png'
-			},
-			{
-				src: '512.png',
-				sizes: '512x512',
-				type: 'image/png'
+				src: 'logo_512.jpeg',
+				type: 'image/png',
+				sizes: '512x512'
 			}
 		]
 	});
+	// return json({
+	// 	name: `Name ${event.params.slug}`,
+	// 	short_name: `S ${event.params.slug}`,
+	// 	description: 'My Awesome App description',
+	// 	theme_color: '#ffffff',
+	// 	icons: [
+	// 		{
+	// 			src: '192.png',
+	// 			sizes: '192x192',
+	// 			type: 'image/png'
+	// 		},
+	// 		{
+	// 			src: '512.png',
+	// 			sizes: '512x512',
+	// 			type: 'image/png'
+	// 		}
+	// 	]
+	// });
 };
