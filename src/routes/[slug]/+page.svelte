f6f5107601fb9ae552f4e6e5709520f55c4b7601
Antoine LB <antoine.stc25@gmail.com>
2024-06-21 15:26:07 -0400

    og test

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,8 +1,28 @@
 <script>
 	export let data;
+	import { MetaTags } from 'svelte-meta-tags';
 </script>
 
-<svelte:head>
+<MetaTags
+	title="Example Title"
+	description="Example Description."
+	openGraph={{
+		url: 'https://www.url.ie/a',
+		title: 'Open Graph Title',
+		description: 'Open Graph Description',
+		images: [
+			{
+				url: 'https://er-s3-prod.s3.fr-par.scw.cloud/pizzo_petr_sevcovic_fy_Qr1_T3_GE_34_unsplash_08bae03d46_27a0655902_a17d57bab8.jpg',
+				width: 2000,
+				height: 1333,
+				alt: 'Og Image Alt'
+			}
+		],
+		siteName: 'SiteName'
+	}}
+/>
+
+<!-- <svelte:head>
 	<meta name="viewport" content="width=device-width,initial-scale=1" />
 	<title>{data.slug} Page</title>
 	<meta name="description" content={`${data.slug} Description`} />
@@ -11,6 +31,6 @@
 	<link rel="mask-icon" href="/mask-icon.svg" color="#FFFFFF" />
 	<meta name="theme-color" content="#ffffff" />
 	<link rel="manifest" href={`/manifest/${data.slug}`} />
-</svelte:head>
+</svelte:head> -->
 
 <h1>{data.slug}</h1>
